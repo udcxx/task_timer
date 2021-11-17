c8eb08942f0f298a635d02c1730fb0e628264c1e
udcxx <me@udcxx.me>
2021-11-17 14:26:06 +0900

    #5 スタイルシート適用

--- src/assets/js/list.js
+++ src/assets/js/list.js
@@ -1,7 +1,7 @@
 // リスト追加
 function addList() {
     const el = document.createElement('li');
-    el.innerHTML = '<button type="button" name="remove-list" onclick="removeList(this)">削除</button><input type="text" placeholder="タスクを入力してください">';
+    el.innerHTML = '<button type="button" name="remove-list" onclick="removeList(this)" class="todo-remove-btn">🗑</button><input type="text" placeholder="タスクを入力してください">';
     el.classList.add('list-content');
 
     document.querySelector('ul.list-contents').appendChild(el);
